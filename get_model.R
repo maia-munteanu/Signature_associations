03c83023e549ee11d84341a13e48d1e557ebcc7f
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2024-07-23 17:11:29 +0200

    Update get_model.R

--- get_model.R
+++ get_model.R
@@ -15,12 +15,12 @@ germline[which(germline$Signatures==signature),] -> germline
 
 results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
 for (gene in unique(germline$Gene.refGene)){
-    df<-germline[which(germline$Gene.refGene == gene & germline$Signatures == sig),]
+    df<-germline[which(germline$Gene.refGene == gene & germline$Signatures == signature),]
     df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
     model <- glm.nb(Exposures ~ Mutation_Score, data = df)
     beta <- coef(model)["Mutation_Score"]
     se <- summary(model)$coefficients["Mutation_Score", "Std. Error"]
     p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|t|)"]
-    results<-rbind(results,data.frame(Signature = sig, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
+    results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
     results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
-    write.table(results, file = paste0("GLMnb_", output_info,"_", sig, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+    write.table(results, file = paste0("GLMnb_", output_info,"_", signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
