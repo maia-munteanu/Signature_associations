4ec4398305fb31b77c1b2a9b33e75a0468646a31
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2024-07-23 17:18:19 +0200

    Update get_model.R

--- get_model.R
+++ get_model.R
@@ -20,7 +20,7 @@ for (gene in unique(germline$Gene.refGene)){
     model <- glm.nb(Exposures ~ Mutation_Score, data = df)
     beta <- coef(model)["Mutation_Score"]
     se <- summary(model)$coefficients["Mutation_Score", "Std. Error"]
-    p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|t|)"]
+    p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|z|)"]
     results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
     results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
     write.table(results, file = paste0("GLMnb_", output_info,"_", signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
