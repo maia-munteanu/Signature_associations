fe26bc85d1104a9a5fba2c4813618d584a8b2d0a
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2024-07-24 13:52:25 +0200

    Update get_model.R

--- get_model.R
+++ get_model.R
@@ -12,14 +12,27 @@ output_info <- as.character(args[3])
 germline=as.data.frame(fread(input))
 germline %>% pivot_longer(cols = -c(sample, Gene.refGene, Freq), names_to = "Signature", values_to = "Exposures") %>% filter(Signature==signature) -> germline
 
-results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
-for (gene in unique(germline$Gene.refGene)){
-    df<-germline[which(germline$Gene.refGene == gene),]
-    df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
-    model <- glm.nb(Exposures ~ Mutation_Score, data = df)
-    beta <- coef(model)["Mutation_Score"]
-    se <- summary(model)$coefficients["Mutation_Score", "Std. Error"]
-    p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|z|)"]
-    results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
-    results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
-    write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+if (model=="GLMnb") {
+    results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
+    for (gene in unique(germline$Gene.refGene)){
+        df<-germline[which(germline$Gene.refGene == gene),]
+        df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
+        model <- glm.nb(Exposures ~ Mutation_Score, data = df)
+        beta <- coef(model)["Mutation_Score"]
+        se <- summary(model)$coefficients["Mutation_Score", "Std. Error"]
+        p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|z|)"]
+        results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
+        results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+        write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+} else if (model=="LM"){
+        results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
+    for (gene in unique(germline$Gene.refGene)){
+        df<-germline[which(germline$Gene.refGene == gene),]
+        df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
+        model <- lm(Exposures ~ Mutation_Score, data = df)
+        beta <- coef(model)["Mutation_Score"]
+        se <- summary(model)$coefficients["Mutation_Score", "Std. Error"]
+        p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|t|)"]
+        results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
+        results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+}
