36b018ee476073d297b08b6be6fb495e196f1ddc
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2024-08-01 15:26:01 +0200

    Update get_model.R

--- get_model.R
+++ get_model.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(foreach)
 library(doParallel)
 library(MASS)
+library(pscl)
 
 args=commandArgs(TRUE)
 signature <- as.character(args[1])
@@ -24,7 +25,9 @@ if (model=="GLMnb") {
         results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
         results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
         write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
-} else if (model=="LM"){
+} 
+
+if (model=="LM"){
     results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
     for (gene in unique(germline$Gene.refGene)){
         df<-germline[which(germline$Gene.refGene == gene),]
@@ -33,6 +36,33 @@ if (model=="GLMnb") {
         beta <- coef(model)["Mutation_Score"]
         se <- summary(model)$coefficients["Mutation_Score", "Std. Error"]
         p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|t|)"]
+        results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
+        results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+}
+
+if (model=="ZInb"){
+    results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
+    for (gene in unique(germline$Gene.refGene)){
+        df<-germline[which(germline$Gene.refGene == gene),]
+        df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
+        model <- zeroinfl(Exposures ~ Mutation_Score | Mutation_Score, data = data, dist = "negbin")
+        beta <- coef(model)["count_Mutation_Score"]
+        se <- summary(model)$coefficients$count["Mutation_Score", "Std. Error"]
+        p_value <- summary(model)$coefficients$count["Mutation_Score", "Pr(>|z|)"]
+        results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
+        results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+}
+
+if (model=="Hnb"){
+    results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
+    for (gene in unique(germline$Gene.refGene)){
+        df<-germline[which(germline$Gene.refGene == gene),]
+        df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
+
+        model <- hurdle(Exposures ~ Mutation_Score | Mutation_Score, data = data, dist = "negbin")
+        beta <- coef(model)["count_Mutation_Score"]
+        se <- summary(model)$coefficients$count["Mutation_Score", "Std. Error"]
+        p_value <- summary(model)$coefficients$count["Mutation_Score", "Pr(>|z|)"]
         results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
         results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
 }
