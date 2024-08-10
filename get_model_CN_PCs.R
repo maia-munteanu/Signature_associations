ff903a7a000499b4ca2d5e8c1e50bca408f65940
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2024-08-10 17:30:45 +0200

    Update get_model_CN_PCs.R

--- get_model_CN_PCs.R
+++ get_model_CN_PCs.R
@@ -32,6 +32,48 @@ if (covariates){
       results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
       write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
   }
+
+  if (model_type=="GLMg"){
+      results=data.frame(Signature = c(), PC = c(), Beta = c(), SE = c(), P_Value = c())
+      for (PC in unique(cna_pcs$PC)){
+        df<-cna_pcs[which(cna_pcs$PC == PC),]
+        df$LizaCancerType[df$LizaCancerType %in%  names(table(df$LizaCancerType)[table(df$LizaCancerType) < 10])] <- "Other"
+        model <- glm(Exposures ~ PC_value + LizaCancerType + gender + purity + tmbStatus + msStatus, data = df, family = gaussian())
+        beta <- coef(model)["PC_value"]
+        se <- summary(model)$coefficients["PC_value", "Std. Error"]
+        p_value <- summary(model)$coefficients["PC_value", "Pr(>|z|)"]
+        results<-rbind(results,data.frame(Signature = signature, PC = PC, Beta = beta, SE = se, P_Value = p_value))}
+      results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+      write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+  }
+  
+  if (model_type=="GLMgsqrt"){
+      results=data.frame(Signature = c(), PC = c(), Beta = c(), SE = c(), P_Value = c())
+      for (PC in unique(cna_pcs$PC)){
+        df<-cna_pcs[which(cna_pcs$PC == PC),]
+        df$LizaCancerType[df$LizaCancerType %in%  names(table(df$LizaCancerType)[table(df$LizaCancerType) < 10])] <- "Other"
+        model <- glm(sqrt(Exposures) ~ PC_value + LizaCancerType + gender + purity + tmbStatus + msStatus, data = df, family = gaussian())
+        beta <- coef(model)["PC_value"]
+        se <- summary(model)$coefficients["PC_value", "Std. Error"]
+        p_value <- summary(model)$coefficients["PC_value", "Pr(>|z|)"]
+        results<-rbind(results,data.frame(Signature = signature, PC = PC, Beta = beta, SE = se, P_Value = p_value))}
+      results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+      write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+  }
+  
+  if (model_type=="GLMp"){
+      results=data.frame(Signature = c(), PC = c(), Beta = c(), SE = c(), P_Value = c())
+      for (PC in unique(cna_pcs$PC)){
+        df<-cna_pcs[which(cna_pcs$PC == PC),]
+        df$LizaCancerType[df$LizaCancerType %in%  names(table(df$LizaCancerType)[table(df$LizaCancerType) < 10])] <- "Other"
+        model <- glm(Exposures ~ PC_value + LizaCancerType + gender + purity + tmbStatus + msStatus, data = df, family = poisson())
+        beta <- coef(model)["PC_value"]
+        se <- summary(model)$coefficients["PC_value", "Std. Error"]
+        p_value <- summary(model)$coefficients["PC_value", "Pr(>|z|)"]
+        results<-rbind(results,data.frame(Signature = signature, PC = PC, Beta = beta, SE = se, P_Value = p_value))}
+      results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+      write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+  }
   
   if (model_type=="beta"){
       results=data.frame(Signature = c(), PC = c(), Beta = c(), SE = c(), P_Value = c())
@@ -48,35 +90,6 @@ if (covariates){
       results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
       write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
   }
-}else{
-  if (model_type=="GLMnb"){
-    results=data.frame(Signature = c(), PC = c(), Beta = c(), SE = c(), P_Value = c())
-    for (PC in unique(cna_pcs$PC)){
-      df<-cna_pcs[which(cna_pcs$PC == PC),]
-      df$LizaCancerType[df$LizaCancerType %in%  names(table(df$LizaCancerType)[table(df$LizaCancerType) < 10])] <- "Other"
-      model <- glm.nb(Exposures ~ PC_value , data = df)
-      beta <- coef(model)["PC_value"]
-      se <- summary(model)$coefficients["PC_value", "Std. Error"]
-      p_value <- summary(model)$coefficients["PC_value", "Pr(>|z|)"]
-      results<-rbind(results,data.frame(Signature = signature, PC = PC, Beta = beta, SE = se, P_Value = p_value))}
-    results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
-    write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
-  }
   
-  if (model_type=="beta"){
-    results=data.frame(Signature = c(), PC = c(), Beta = c(), SE = c(), P_Value = c())
-    for (PC in unique(cna_pcs$PC)){
-      df<-cna_pcs[which(cna_pcs$PC == PC),]
-      df$LizaCancerType[df$LizaCancerType %in%  names(table(df$LizaCancerType)[table(df$LizaCancerType) < 10])] <- "Other"
-      df$Exposures[df$Exposures == 0] <- df$Exposures[df$Exposures == 0] + 0.00001
-      df$Exposures[df$Exposures == 1] <- df$Exposures[df$Exposures == 1] - 0.00001
-      model <- betareg(Exposures ~ PC_value, data = df)
-      beta <- coef(model)["PC_value"]
-      se <- summary(model)$coefficients$mean["PC_value", "Std. Error"]
-      p_value <- summary(model)$coefficients$mean["PC_value", "Pr(>|z|)"]
-      results<-rbind(results,data.frame(Signature = signature, PC = PC, Beta = beta, SE = se, P_Value = p_value))}
-    results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
-    write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
-  }
 }
 
