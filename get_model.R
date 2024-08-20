6301239a59ed103b14c3ee5b5d82372978941cfd
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2024-08-20 11:53:20 +0200

    Update get_model.R

--- get_model.R
+++ get_model.R
@@ -17,53 +17,71 @@ germline=as.data.frame(fread(input))
 germline %>% pivot_longer(cols = -c(sample, Gene.refGene, Freq), names_to = "Signature", values_to = "Exposures") %>% filter(Signature==signature) -> germline
 germline %>% left_join(metadata[,c("sample","gender","purity","ploidy","msStatus","tmbStatus","primaryTumorLocation")]) -> germline
 
-if (model_type=="GLMnb") {
-    results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
-    for (gene in unique(germline$Gene.refGene)){
-        print(gene)
-        df<-germline[which(germline$Gene.refGene == gene),]
-        df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
-        df$primaryTumorLocation[df$primaryTumorLocation %in%  names(table(df$primaryTumorLocation)[table(df$primaryTumorLocation) < 10])] <- "Other"; df$primaryTumorLocation=factor(df$primaryTumorLocation)
-        model <- glm.nb(Exposures ~ Mutation_Score + primaryTumorLocation + msStatus, data = df)
-        beta <- coef(model)["Mutation_Score"]
-        se <- summary(model)$coefficients["Mutation_Score", "Std. Error"]
-        p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|z|)"]
-        results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
-    results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
-    write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
-} 
-
-if (model_type=="GLMglog2") {
-    results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
-    for (gene in unique(germline$Gene.refGene)){
-        print(gene)
-        df<-germline[which(germline$Gene.refGene == gene),]
-        df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
-        df$primaryTumorLocation[df$primaryTumorLocation %in%  names(table(df$primaryTumorLocation)[table(df$primaryTumorLocation) < 10])] <- "Other"; df$primaryTumorLocation=factor(df$primaryTumorLocation)
-        model <- glm(log2(Exposures + 1) ~ Mutation_Score + primaryTumorLocation + msStatus + tmbStatus + purity + ploidy + gender, family = gaussian(), data = df)
-        beta <- coef(model)["Mutation_Score"]
-        se <- summary(model)$coefficients["Mutation_Score", "Std. Error"]
-        p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|t|)"]
-        results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
-    results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
-    write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
-} 
-
-if (model_type=="beta"){
-    results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
-    for (gene in unique(germline$Gene.refGene)){
-        print(gene)
-        df<-germline[which(germline$Gene.refGene == gene),]
-        df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
-        df$primaryTumorLocation[df$primaryTumorLocation %in%  names(table(df$primaryTumorLocation)[table(df$primaryTumorLocation) < 10])] <- "Other"; df$primaryTumorLocation=factor(df$primaryTumorLocation)
-        df$Exposures[df$Exposures == 0] <- df$Exposures[df$Exposures == 0] + 0.00001
-        df$Exposures[df$Exposures == 1] <- df$Exposures[df$Exposures == 1] - 0.00001
-        model <- betareg(Exposures ~ Mutation_Score + primaryTumorLocation + msStatus + tmbStatus + purity + ploidy + gender, data = df)
-        beta <- coef(model)["Mutation_Score"]
-        se <- summary(model)$coefficients$mean["Mutation_Score", "Std. Error"]
-        p_value <- summary(model)$coefficients$mean["Mutation_Score", "Pr(>|z|)"]
-        results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
-    results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
-    write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+if (covariates) {
+    if (model_type=="GLMglog2") {
+        results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
+        for (gene in unique(germline$Gene.refGene)){
+            print(gene)
+            df<-germline[which(germline$Gene.refGene == gene),]
+            df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
+            df$primaryTumorLocation[df$primaryTumorLocation %in%  names(table(df$primaryTumorLocation)[table(df$primaryTumorLocation) < 10])] <- "Other"; df$primaryTumorLocation=factor(df$primaryTumorLocation)
+            model <- glm(log2(Exposures + 1) ~ Mutation_Score + primaryTumorLocation + msStatus + tmbStatus + purity + ploidy + gender, family = gaussian(), data = df)
+            beta <- coef(model)["Mutation_Score"]
+            se <- summary(model)$coefficients["Mutation_Score", "Std. Error"]
+            p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|t|)"]
+            results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
+        results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+        write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+    } 
+    
+    if (model_type=="beta"){
+        results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
+        for (gene in unique(germline$Gene.refGene)){
+            print(gene)
+            df<-germline[which(germline$Gene.refGene == gene),]
+            df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
+            df$primaryTumorLocation[df$primaryTumorLocation %in%  names(table(df$primaryTumorLocation)[table(df$primaryTumorLocation) < 10])] <- "Other"; df$primaryTumorLocation=factor(df$primaryTumorLocation)
+            df$Exposures[df$Exposures == 0] <- df$Exposures[df$Exposures == 0] + 0.00001
+            df$Exposures[df$Exposures == 1] <- df$Exposures[df$Exposures == 1] - 0.00001
+            model <- betareg(Exposures ~ Mutation_Score + primaryTumorLocation + msStatus + tmbStatus + purity + ploidy + gender, data = df)
+            beta <- coef(model)["Mutation_Score"]
+            se <- summary(model)$coefficients$mean["Mutation_Score", "Std. Error"]
+            p_value <- summary(model)$coefficients$mean["Mutation_Score", "Pr(>|z|)"]
+            results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
+        results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+        write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+    }
+} else {
+    if (model_type=="GLMglog2") {
+        results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
+        for (gene in unique(germline$Gene.refGene)){
+            print(gene)
+            df<-germline[which(germline$Gene.refGene == gene),]
+            df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
+            df$primaryTumorLocation[df$primaryTumorLocation %in%  names(table(df$primaryTumorLocation)[table(df$primaryTumorLocation) < 10])] <- "Other"; df$primaryTumorLocation=factor(df$primaryTumorLocation)
+            model <- glm(log2(Exposures + 1) ~ Mutation_Score, family = gaussian(), data = df)
+            beta <- coef(model)["Mutation_Score"]
+            se <- summary(model)$coefficients["Mutation_Score", "Std. Error"]
+            p_value <- summary(model)$coefficients["Mutation_Score", "Pr(>|t|)"]
+            results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
+        results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+        write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+    } 
+    if (model_type=="beta"){
+        results=data.frame(Signature = c(), Gene = c(), Beta = c(), SE = c(), P_Value = c())
+        for (gene in unique(germline$Gene.refGene)){
+            print(gene)
+            df<-germline[which(germline$Gene.refGene == gene),]
+            df$Mutation_Score <- ifelse(df$Freq > 0, 1, 0)
+            df$primaryTumorLocation[df$primaryTumorLocation %in%  names(table(df$primaryTumorLocation)[table(df$primaryTumorLocation) < 10])] <- "Other"; df$primaryTumorLocation=factor(df$primaryTumorLocation)
+            df$Exposures[df$Exposures == 0] <- df$Exposures[df$Exposures == 0] + 0.00001
+            df$Exposures[df$Exposures == 1] <- df$Exposures[df$Exposures == 1] - 0.00001
+            model <- betareg(Exposures ~ Mutation_Score, data = df)
+            beta <- coef(model)["Mutation_Score"]
+            se <- summary(model)$coefficients$mean["Mutation_Score", "Std. Error"]
+            p_value <- summary(model)$coefficients$mean["Mutation_Score", "Pr(>|z|)"]
+            results<-rbind(results,data.frame(Signature = signature, Gene = gene, Beta = beta, SE = se, P_Value = p_value))}
+        results$Adjusted_P_Value <- p.adjust(results$P_Value, method = "BH")
+        write.table(results, file = paste0(signature, ".tsv"),quote = FALSE, row.names = FALSE, col.names = TRUE, sep = "\t")
+    }
 }
-
